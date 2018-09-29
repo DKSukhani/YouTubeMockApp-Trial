bcb67b4e800f262b6d75ecea841e965d067d5f5e
Dipesh Sukhani <dipeshsukhani@gmail.com>
2018-09-29 12:54:20 +0800

    udpated the searchbar for setState

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -22,13 +22,37 @@ import React, { Component } from 'react';
 //   return <input />
 // };
 
-// DKS Comment: the below is a class based component and knows what is being inputed
+// DKS Comment: the below is a class based component and knows what is being inputed with the state
+
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+  }
+
+
   render() {
-    return <input onChange={(event) => { console.log(event.target.value) }} />;
+    return (
+      <div>
+        <input onChange={(event) => this.setState({ term: event.target.value })} />
+        Value of the input is: {this.state.term}
+      </div >);
+
+    // return <input onChange={(event) => { console.log(event.target.value) }} />;
   }
 
 }
+
+
+// based on the syntax: setState is a function / method that uses object as its argument
+
+// class SearchBar extends Component {
+//   render() {
+//     return <input onChange={(event) => { console.log(event.target.value) }} />;
+//   }
+
+// }
 
 // class SearchBar extends Component {
 //   render() {
