f43d9e84da58825b45aa2ebb90b7dab8c4bc7ede
Dipesh Sukhani <dipeshsukhani@gmail.com>
2018-09-29 13:16:10 +0800

    Controlled Form
    We updated the SearchBar function to a controlled form, in which we set the value of the form as the term while we initalize it

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -35,7 +35,10 @@ class SearchBar extends Component {
   render() {
     return (
       <div>
-        <input onChange={(event) => this.setState({ term: event.target.value })} />
+        <input
+          value={this.state.term}
+          onChange={(event) => this.setState({ term: event.target.value })} />
+        <br />
         Value of the input is: {this.state.term}
       </div >);
 
